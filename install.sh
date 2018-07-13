bb6ceefd861eb7bdd818f76cfbda3c01fb6b7c1d
Khalah Jones Golden <khasan222@gmail.com>
2018-07-13 11:35:43 -0400

    [Install] Fixed the github repos to make it so they all install fine without an ssh key

--- install.sh
+++ install.sh
@@ -24,12 +24,12 @@ echo downloaded pathogen
 echo downloading git repos for plugins
 cd ~/.config/nvim/bundle
 echo cloning ack
-git clone git@github.com:mileszs/ack.vim.git
+git clone https://github.com/mileszs/ack.vim.git
 echo cloning ctrlp
-git clone git@github.com:kien/ctrlp.vim.git
+git clone https://github.com/kien/ctrlp.vim.git
 echo cloning nerdtree
-git clone git@github.com:scrooloose/nerdtree.git
+git clone https://github.com/scrooloose/nerdtree.git
 echo cloning superTab
-git clone git@github.com:ervandew/supertab.git
+git clone https://github.com/ervandew/supertab.git
 echo cloning solarized
-git clone git@github.com:altercation/vim-colors-solarized.git
+git clone https://github.com/altercation/vim-colors-solarized.git
