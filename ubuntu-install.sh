e4145fd1c37e829b68c066dcb07e37fe341f94a2
Khalah Jones-Golden <khasan222@gmail.com>
2019-01-10 19:02:49 -0400

    [GIT] added some git color settings

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -2,5 +2,6 @@ sudo apt install git neovim curl tmux ack
 
 git config --global user.email "khasan222@gmail.com"
 git config --global user.name "Khalah Jones-Golden"
+git config --global color.ui auto
 
 sh install.sh
